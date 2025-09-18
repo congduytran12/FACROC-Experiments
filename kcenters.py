84686a67a3a90d9c08cc1fdb6a248cf09b739ef6
Tran Cong Duy <congduytran12@gmail.com>
2025-09-18 14:35:41 +0700

    use original kcenters algorithm

--- kcenters.py
+++ kcenters.py
@@ -1,6 +1,6 @@
 import random
-from utils import distance
 import numpy as np
+from utils import distance
 
 class KCenters(object):
     def __init__(self, k=2):
@@ -11,68 +11,29 @@ class KCenters(object):
 
     def fit(self, data):
         """
-        Performs the enhanced k-centers algorithm with k-means++ initialization.
+        Performs the k-centers algorithm.
 
         Args:
             data (list) : Points in the dataset
         """
+        # choose initial center randomly
         random.seed(42)
-        np.random.seed(42)
 
         self.data = data
-        self.centers = []
+        self.centers = [np.random.randint(0, len(self.data))]
         self.costs = []
         
-        if len(self.data) == 0:
-            return
-            
-        # k-means++ initialization for better center selection
-        # choose first center randomly
-        first_center = np.random.randint(0, len(self.data))
-        self.centers.append(first_center)
-
-        # choose remaining centers using k-means++ logic
-        for _ in range(self.k - 1):
-            if len(self.centers) >= len(self.data):
-                break
-                
-            # calculate distances from each point to nearest center
-            distances = []
+        while True:
+            # remain points in the dataset
             rem_points = list(set(range(0, len(self.data))) - set(self.centers))
-            
-            for i in rem_points:
-                min_dist = min([distance(self.data[i], self.data[j]) for j in self.centers])
-                distances.append((i, min_dist))
-            
-            if not distances:
-                break
-                
-            # choose next center with probability proportional to squared distance
-            distances = sorted(distances, key=lambda x: x[1], reverse=True)
-            weights = [d[1]**2 for d in distances]
-            total_weight = sum(weights)
-            
-            if total_weight > 0:
-                probabilities = [w / total_weight for w in weights]
-                
-                # select based on weighted probability
-                rand_val = np.random.random()
-                cumsum = 0
-                next_center = distances[0][0]  
-                
-                for i, (point_idx, _) in enumerate(distances):
-                    cumsum += probabilities[i]
-                    if rand_val <= cumsum:
-                        next_center = point_idx
-                        break
-                        
-                self.centers.append(next_center)
-                self.costs.append(distances[0][1])  # max distance for this iteration
+            # find point with maximum distance to its closest center
+            point_center = [(i, min([distance(self.data[i], self.data[j]) for j in self.centers])) for i in rem_points]
+            point_center = sorted(point_center, key=lambda x: x[1], reverse=True)
+            self.costs.append(point_center[0][1])
+            if len(self.centers) < self.k:
+                self.centers.append(point_center[0][0])
             else:
-                # if all distances are 0, just pick the first remaining point
-                self.centers.append(distances[0][0])
-                self.costs.append(0)
-        
+                break
         return
 
     def assign(self):
