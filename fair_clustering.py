0faa70639a834c487d6559043a83729b2d34690b
Tran Cong Duy <congduytran12@gmail.com>
2025-10-10 10:28:07 +0700

    run scalable fair clustering on all datasets

--- fair_clustering.py
+++ fair_clustering.py
@@ -9,9 +9,9 @@ DATASET_CONFIGS = {
     'student-mat-encode.csv': {'k': 9, 'p': 2, 'q': 5},    
     'student-por-encode.csv': {'k': 9, 'p': 3, 'q': 6},
     'german-encode.csv': {'k': 2, 'p': 2, 'q': 5},
-    # 'compas-encode.csv': {'k': 7, 'p': 2, 'q': 5},
-    # 'credit-encode.csv': {'k': 2, 'p': 2, 'q': 5},
-    # 'adult-encode.csv': {'k': 2, 'p': 2, 'q': 5}
+    'compas-encode.csv': {'k': 7, 'p': 2, 'q': 5},
+    'credit-encode.csv': {'k': 2, 'p': 2, 'q': 5},
+    'adult-encode.csv': {'k': 2, 'p': 2, 'q': 5}
 }
 
 PROTECTED_ATTRIBUTES = {
